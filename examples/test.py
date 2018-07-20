e2e3d5bd67fc639e964f4a4fa455e685778a5d3c
Dmitry Ischenko <ischenko.dmitry@gmail.com>
2018-07-20 12:08:55 +0300

    change seq2seq to simple rnn model

---
+++ examples/test.py
@@ -0,0 +1,3 @@
+from vgenerator.models import Generator
+
+model = Generator(10, 10, 10, 2)
--- vgenerator/models/Generator.py
+++ vgenerator/models/Generator.py
@@ -1,5 +1,25 @@
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
-class Generator(object):
-    pass
+class Generator(nn.Module):
+    def __init__(self, num_tokens, emb_size, hidden_size, num_layers):
+        super(self.__class__, self).__init__()
+
+        self.embedding = nn.Embedding(num_tokens, emb_size)
+
+        self.rnn = nn.LSTM(
+            emb_size,
+            hidden_size,
+            num_layers
+        )
+
+        self.out = nn.Linear(hidden_size, num_tokens)
+
+
+    def forward(self, input):
+        output, _ = self.rnn(self.embedding(input))
+        output = self.out(output)
+
+        return F.log_softmax(output, dim=-1)
